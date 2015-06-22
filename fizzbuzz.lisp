e74a3f3f30729debfebf3dceb45465598043eb9c
haretaro <frontend.1000@gmail.com>
2015-06-23 04:19:34 +0900

    fixed the part to print

--- fizzbuzz.lisp
+++ fizzbuzz.lisp
@@ -1,8 +1,9 @@
 (defun fizzbuzz (n end) (if (> n end) nil (cons
 	(cond
-	((= 0 (mod n 15)) (print "fizzbuzz"))
-	((= 0 (mod n 3)) (print "fizz"))
-	((= 0 (mod n 5)) (print "buzz"))
-	(t (print n))) (fizzbuzz (+ n 1) end))))
+		((= 0 (mod n 15)) "fizzbuzz")
+		((= 0 (mod n 3))  "fizz")
+		((= 0 (mod n 5)) "buzz")
+		(t n))
+	(fizzbuzz (+ n 1) end))))
 
-(print (fizzbuzz 1 100))
+(format t "~{~A ~}" (fizzbuzz 1 100))
