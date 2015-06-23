dc7d514cfc1422eb0f8da73640b2e35b41b10708
haretaro <frontend.1000@gmail.com>
2015-06-23 23:30:05 +0900

    rewote fibonacci with memoization

--- fibonacci.lisp
+++ fibonacci.lisp
@@ -1,13 +1,15 @@
-(defun fibonacci (end memo)
-  (if (> 2 (length memo))
-    (fibonacci end
-               (cons (print 1) memo))
-    (if (< end (length memo))
-      memo
-      (fibonacci end
-                 (cons (print
-                         (+ (car memo)
-                            (car (cdr memo))
-                            )) memo)))))
+(defparameter memo (make-hash-table))
 
-(fibonacci 1000 nil)
+(defun fibonacci (n)
+  (if (gethash n memo)
+    (gethash n memo)
+    (if (> 3 n)
+      (setf (gethash n memo) 1)
+      (setf (gethash n memo)
+            (+ (fibonacci (- n 1)) (fibonacci (- n 2)))))))
+
+(setq n 1)
+(loop
+  (print (fibonacci n))
+  (setq n (+ n 1))
+  (if (> n 1000) (return t)))
