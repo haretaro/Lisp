e59ec5cc0f382de2a9348ab0c8e6fd4d57ec08b7
haretaro <frontend.1000@gmail.com>
2015-07-13 14:03:45 +0900

    add extend method

--- mylist.lisp
+++ mylist.lisp
@@ -26,9 +26,9 @@
     (setf (node-data (mylist-end self)) (car data-list))
     (setf (node-next (mylist-end self)) (make-instance 'node))
     (setf (mylist-end self) (node-next (mylist-end self)))
-    (if (equal (cdr data-list) nil)
-      nil
-      (add-data (cdr data-list))))
+    (if (cdr data-list)
+      (add-data (cdr data-list))
+      nil))
   (add-data data)
   self)
 
@@ -39,22 +39,22 @@
 (defmethod print-object ((self mylist) out)
   (print-unreadable-object (self out :type t)
     (defun print-node (somenode)
-      (if (equal (node-next somenode) nil)
-        nil
+      (if (node-next somenode)
         (progn
           (format out "~s" (node-data somenode))
-          (if (equal (node-next (node-next somenode)) nil)
-            nil
-            (format out  ","))
-          (print-node (node-next somenode)))))
+          (if (node-next (node-next somenode))
+            (format out  ",")
+            nil)
+          (print-node (node-next somenode)))
+        nil))
     (print-node (mylist-head self))))
 
 (defmethod mylist-len
   ((self mylist))
    (defun node-len(somenode)
-     (if (equal (node-next somenode) nil)
-       0
-       (+ 1 (node-len (node-next somenode)))))
+     (if (node-next somenode)
+       (+ 1 (node-len (node-next somenode)))
+       0))
    (node-len (mylist-head self)))
 
 (defmethod mylist-get
@@ -64,3 +64,25 @@
       (node-data somenode)
       (getdata (node-next somenode) (- num 1))))
   (getdata (mylist-head self) index))
+
+(defmethod mylist-clone ((self mylist))
+  (let (newlist)
+    (setq newlist (make-instance 'mylist))
+    (setq node (mylist-head self))
+    (loop
+      (if (node-next node)
+        (progn
+          (mylist-append newlist (node-data node))
+          (setq node (node-next node)))
+        (return newlist)))))
+
+(defmethod mylist-extend ((self mylist) (li mylist))
+  (let (newlist node)
+    (setq newlist (mylist-clone self))
+    (setq node (mylist-head li))
+    (loop
+      (if (node-next node)
+        (progn
+          (mylist-append newlist (node-data node))
+          (setq node (node-next node)))
+        (return newlist)))))
